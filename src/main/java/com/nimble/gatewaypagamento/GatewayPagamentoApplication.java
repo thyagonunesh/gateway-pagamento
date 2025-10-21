3458cb5610e1870e81b659d921813e7591a14cee
Thyago Nunes <thyago.nunes@sigep.com.br>
2025-10-21 10:30:18 -0300

    Criando estrutura inicial do projeto

---
+++ src/main/java/com/nimble/gatewaypagamento/GatewayPagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.nimble.gatewaypagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPagamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayPagamentoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nimble/gatewaypagamento/GatewayPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimble.gatewaypagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayPagamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
